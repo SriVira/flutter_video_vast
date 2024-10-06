77b2bfdf53543c274bdca07d5bfa1b788c2bf188
Lakshitha Perera <lakshitha1629@gmail.com>
2024-10-06 22:03:31 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'video_player_with_vast.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter VAST Video Player')),
+        body: CustomVideoPlayer(
+          videoUrl: 'https://vz-6c38baf6-966.b-cdn.net/f6e6315e-e269-4993-a2dd-763cc6f39644/playlist.m3u8',
+          vastEnabled: true,
+          vastTagPreroll: 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_preroll_skippable&sz=640x480&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=',
+          vastTagMidroll: 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=',
+          vastTagPostroll: 'https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=',
+          midRollDuration: 2,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/video_player_with_vast.dart
@@ -0,0 +1,262 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+import 'package:http/http.dart' as http;
+import 'package:xml/xml.dart' as xml;
+import 'package:url_launcher/url_launcher.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final String videoUrl;
+  final bool vastEnabled;
+  final String vastTagPreroll;
+  final String vastTagMidroll;
+  final String vastTagPostroll;
+  final int midRollDuration;
+
+  CustomVideoPlayer({
+    required this.videoUrl,
+    this.vastEnabled = false, // VAST ads disabled by default
+    required this.vastTagPreroll,
+    required this.vastTagMidroll,
+    required this.vastTagPostroll,
+    this.midRollDuration = 0,
+  });
+
+  @override
+  _CustomVideoPlayerState createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  late VideoPlayerController _mainController;
+  ChewieController? _chewieController; // Chewie controller for enhanced UI
+  VideoPlayerController? _adController;
+  bool _isPlayingAd = false;
+  bool _isMidRollPlayed = false;
+  bool _isMainControllerInitialized = false;
+  bool _isAdControllerInitialized = false;
+  bool _isSkipVisible = false;
+  String? _adClickThroughUrl;
+  int _skipTime = 5; // Number of seconds before "Skip Ad" is shown
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.vastEnabled) {
+      playPrerollAd();
+    } else {
+      _initializeMainVideo();
+    }
+  }
+
+  @override
+  void dispose() {
+    _mainController.dispose();
+    _adController?.dispose();
+    _chewieController?.dispose(); // Dispose of Chewie controller
+    super.dispose();
+  }
+
+  void _initializeMainVideo() {
+    _mainController = VideoPlayerController.network(widget.videoUrl)
+      ..initialize().then((_) {
+        if (mounted) {
+          _createChewieController();
+          setState(() {
+            _isMainControllerInitialized = true;
+          });
+        }
+        _mainController.play();
+        _mainController.addListener(_checkMidRollAd);
+        _mainController.addListener(_checkPostRollAd);
+      });
+  }
+
+  // Initialize Chewie with the main controller
+  void _createChewieController() {
+    _chewieController = ChewieController(
+      videoPlayerController: _mainController,
+      autoPlay: true,
+      looping: false,
+      showControls: true,
+      allowMuting: true,
+      allowFullScreen: true,
+      materialProgressColors: ChewieProgressColors(
+        playedColor: Colors.red,
+        handleColor: Colors.red,
+        backgroundColor: Colors.grey,
+        bufferedColor: Colors.lightGreen,
+      ),
+      placeholder: Container(
+        color: Colors.black,
+      ),
+    );
+  }
+
+  Future<void> playPrerollAd() async {
+    final adUrl = await _getVastAd(widget.vastTagPreroll);
+    if (adUrl != null) {
+      playAd(adUrl, _initializeMainVideo);
+    } else {
+      _initializeMainVideo();
+    }
+  }
+
+  Future<void> playMidrollAd() async {
+    if (!_isMidRollPlayed && widget.vastEnabled) {
+      final adUrl = await _getVastAd(widget.vastTagMidroll);
+      if (adUrl != null) {
+        _mainController.pause();
+        _isMidRollPlayed = true;
+        playAd(adUrl, () {
+          _mainController.play();
+        });
+      }
+    }
+  }
+
+  Future<void> playPostrollAd() async {
+    final adUrl = await _getVastAd(widget.vastTagPostroll);
+    if (adUrl != null) {
+      _mainController.pause();
+      playAd(adUrl, () {
+        _mainController.play(); // Continue main video after postroll
+      });
+    }
+  }
+
+  Future<void> playAd(String adUrl, VoidCallback onAdComplete) async {
+    if (_adController != null) {
+      _adController!.dispose(); // Dispose previous ad controller if exists
+    }
+
+    _adController = VideoPlayerController.network(adUrl)
+      ..initialize().then((_) {
+        if (mounted) {
+          setState(() {
+            _isAdControllerInitialized = true;
+            _isPlayingAd = true;
+            _startSkipTimer(); // Start skip ad countdown
+          });
+        }
+        _adController!.play();
+        _adController!.addListener(() {
+          if (_adController!.value.position == _adController!.value.duration) {
+            onAdComplete();
+            _adController!.dispose();
+            setState(() {
+              _isAdControllerInitialized = false;
+              _isPlayingAd = false;
+            });
+          }
+        });
+      });
+  }
+
+  Future<String?> _getVastAd(String vastUrl) async {
+    try {
+      final response = await http.get(Uri.parse(vastUrl));
+      if (response.statusCode == 200) {
+        final document = xml.XmlDocument.parse(response.body);
+        final mediaFiles = document.findAllElements('MediaFile');
+        final clickThrough = document.findAllElements('ClickThrough');
+        if (clickThrough.isNotEmpty) {
+          _adClickThroughUrl = clickThrough.first.text.trim();
+        }
+        if (mediaFiles.isNotEmpty) {
+          return mediaFiles.first.text.trim();
+        }
+      }
+    } catch (e) {
+      print('Error fetching VAST ad: $e');
+    }
+    return null;
+  }
+
+  void _startSkipTimer() {
+    Future.delayed(Duration(seconds: _skipTime), () {
+      if (mounted) {
+        setState(() {
+          _isSkipVisible = true; // Show "Skip Ad" button after 5 seconds
+        });
+      }
+    });
+  }
+
+  void _skipAd() {
+    // Check if the ad controller is initialized before disposing
+    if (_adController != null) {
+      _adController!.dispose(); // Dispose the ad controller if it's initialized
+      _adController = null; // Set it to null to avoid reuse
+    }
+
+    setState(() {
+      _isSkipVisible = false; // Hide the skip button
+      _isPlayingAd = false; // No longer playing an ad
+      _isAdControllerInitialized = false;
+    });
+
+    // Ensure the main video controller resumes playing after skipping the ad
+    if (_mainController.value.isInitialized) {
+      _mainController.play();
+    }
+  }
+
+  void _checkMidRollAd() {
+    if (_mainController.value.position.inSeconds >= widget.midRollDuration &&
+        !_isPlayingAd) {
+      _isPlayingAd = true;
+      playMidrollAd();
+    }
+  }
+
+  void _checkPostRollAd() {
+    if (_mainController.value.position == _mainController.value.duration) {
+      playPostrollAd();
+    }
+  }
+
+  void _onAdClick() async {
+    if (_adClickThroughUrl != null && await canLaunch(_adClickThroughUrl!)) {
+      await launch(_adClickThroughUrl!,
+          forceSafariVC: false, forceWebView: false);
+    } else {
+      print('Could not launch the ad click-through URL');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Center(
+        child: _isMainControllerInitialized ||
+                (_isAdControllerInitialized && _adController != null)
+            ? Stack(
+                children: [
+                  AspectRatio(
+                    aspectRatio: _isPlayingAd && _adController != null
+                        ? _adController!.value.aspectRatio
+                        : _mainController.value.aspectRatio,
+                    child: _isPlayingAd && _adController != null
+                        ? VideoPlayer(_adController!)
+                        : Chewie(controller: _chewieController!),
+                  ),
+                  if (_isSkipVisible && _isPlayingAd && _adController != null)
+                    Positioned(
+                      top: 10,
+                      right: 10,
+                      child: ElevatedButton(
+                        onPressed: _skipAd,
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white.withOpacity(0.7),
+                          onPrimary: Colors.black,
+                        ),
+                        child: Text('Skip Ad'),
+                      ),
+                    ),
+                ],
+              )
+            : CircularProgressIndicator(),
+      ),
+    );
+  }
+}
